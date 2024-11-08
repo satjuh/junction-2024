e5e8c3fe22608879ea7fb21de3065beb0cebd0e6
Juhani Säteri <satjuh.77@gmail.com>
2024-11-08 22:29:38 +0200

    Clean up and basic skeleton.

---
+++ backend/backend/routes/object3d.py
@@ -0,0 +1,33 @@
+from uuid import UUID
+from fastapi import  Depends, HTTPException
+from sqlalchemy.orm import Session
+from backend import models, schemas
+from ..db import engine, get_db
+from fastapi import APIRouter
+
+import uuid
+
+models.Base.metadata.create_all(bind=engine)
+
+router = APIRouter()
+
+@router.post("/object3ds/", response_model=schemas.Object3D)
+def create_object3d(object3d: schemas.Object3DCreate, db: Session = Depends(get_db)):
+    floor = db.query(models.Floor).filter(models.Floor.uuid == object3d.floor_id).one()
+    db_object3d = models.Object3D(name=object3d.name, uuid=uuid.uuid4(), floor_id = floor.uuid)
+    db.add(db_object3d)
+    db.commit()
+    db.refresh(db_object3d)
+    return db_object3d
+
+
+@router.get("/object3ds/{object3d_id}", response_model=schemas.Object3D)
+def read_object3d(object3d_id: UUID, db: Session = Depends(get_db)):
+    db_object3d = db.query(models.Object3D).filter(models.Object3D.uuid == object3d_id).first()
+    if db_object3d is None:
+        raise HTTPException(status_code=404, detail="Object3D not found")
+    return db_object3d
+
+@router.get("/object3ds/", response_model=list[schemas.Object3D])
+def read_object3ds(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return db.query(models.Object3D).offset(skip).limit(limit).all()
